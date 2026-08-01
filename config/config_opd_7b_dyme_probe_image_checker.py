8cdf9d9da0e93dc1d6126829aa5ea60e6d587e79
Junjie Yang <yangjj007@foxmail.com>
2026-08-01 23:12:24 +0800

    Update config_opd_7b_dyme_probe_image_checker.py

--- config/config_opd_7b_dyme_probe_image_checker.py
+++ config/config_opd_7b_dyme_probe_image_checker.py
@@ -12,7 +12,7 @@ import os
 
 import config.config_opd_7b_dyme_probe as base
 from config.env_overrides import env_bool, env_str
-from data_utils.paths import OUTPUTS_DIR
+from data_utils.paths import OUTPUTS_DIR, project_path
 
 OUTPUT_DIR = env_str(
     "DYME_OUTPUT_DIR",
@@ -21,13 +21,21 @@ OUTPUT_DIR = env_str(
 
 CONFIG = copy.deepcopy(base.CONFIG)
 CONFIG["training"]["dyme_args"]["output_dir"] = OUTPUT_DIR
+CONFIG["dataset"]["train_dataset"] = project_path("data", "chartqa", "train_new_prerefine.json")
 
 _visual = CONFIG["opsd"].setdefault("visual_supervision", {})
 _checker = _visual.setdefault("checker", {})
 _checker["enabled"] = env_bool("DYME_VISUAL_CHECKER", True)
+_checker["model_source"] = env_str("DYME_VISUAL_CHECKER_MODEL_SOURCE", "loaded_teacher")
 _checker["grounding"] = env_str("DYME_VISUAL_CHECKER_GROUNDING", "image_primary")
 _checker["aux_evidence"] = env_str("DYME_VISUAL_CHECKER_AUX", "none")
+
+_refiner = _visual.setdefault("refiner", {})
+_refiner["enabled"] = env_bool("DYME_VISUAL_REFINER", True)
+_refiner["model_source"] = env_str("DYME_VISUAL_REFINER_MODEL_SOURCE", "loaded_teacher")
+_refiner["fallback"] = env_str("DYME_VISUAL_REFINER_FALLBACK", "passthrough")
+
 _visual["enabled"] = bool(
     _checker.get("enabled")
-    or _visual.get("refiner", {}).get("enabled", False)
+    or _refiner.get("enabled", False)
 )
